7589e4bd6ba76d25978e670ef1020df049be02c4
hemavarshiniloya <112843810+hemavarshiniloya@users.noreply.github.com>
2022-09-05 10:49:45 +0530

    Update firstnew.py

--- firstnew.py
+++ firstnew.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.hearder('RAMACHANDRA COLLEGE OF ENGINEERING')
+st.header('RAMACHANDRA COLLEGE OF ENGINEERING')
 a=st.number_input('enter any value')
 if st.button('HIT ME'):
   st.success(f'your lucky number is {a}')
